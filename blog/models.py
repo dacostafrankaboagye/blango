8bbe8d856d1e0696187391c4f54be67c31e4dc49
Frank Aboagye <frank.aboagye@ashesi.edu.gh>
2025-10-03 13:55:34 +0000

    reverse generic relationship

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.fields import GenericRelation
 
 # Create your models here.
 
@@ -26,8 +27,9 @@ class Post(models.Model):
   title = models.TextField(max_length=100)
   slug = models.SlugField()
   summary = models.TextField(max_length=500)
-  content = models.TextField()
+  content = models.TextField(default="")
   tags = models.ManyToManyField(Tag, related_name="posts")
+  comments = GenericRelation(Comment)
 
   def __str__(self):
     return self.title
