f2c364628d4768b6a4d4bbe46cea0929efe79026
Frank Aboagye <frank.aboagye@ashesi.edu.gh>
2025-10-03 14:08:54 +0000

    blango project update

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.fields import GenericRelation
+from django.utils import timezone
 
 # Create your models here.
 
@@ -18,6 +19,8 @@ class Comment(models.Model):
   content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
   object_id = models.PositiveIntegerField()
   content_object = GenericForeignKey("content_type", "object_id" )
+  created_at = models.DateTimeField(auto_now_add=True)
+  modified_at = models.DateTimeField(auto_now=True)
 
 class Post(models.Model):
   author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
