84eb31b382392dd364e33b9aecdcd0b8755ac2f2
Frank Aboagye <frank.aboagye@ashesi.edu.gh>
2025-10-03 11:19:38 +0000

    admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import Tag, Post
+
+admin.site.register(Tag, PostAdmin)
+
+class PostAdmin(admin.ModelAdmin):
+  prepopulated_fields = {"slug": ("title",)}
